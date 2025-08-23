03f8ebbbe40171de15de6ccbbbf2724e5b477614
Pavitra Sharma <pavitrasharma2306@gmail.com>
2025-08-23 16:26:16 +0530

    Update database.py

--- database.py
+++ database.py
@@ -21,6 +21,7 @@ def get_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
         dbname=os.getenv("DB_NAME")
+        port="5432"
     )
 
 def get_embedding(text):
