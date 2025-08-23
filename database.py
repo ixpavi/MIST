3ac8f0cc069cdc3283713b2adf8479bf973d2b10
Pavitra Sharma <pavitrasharma2306@gmail.com>
2025-08-23 16:45:41 +0530

    Update database.py

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ def get_connection():
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        port=os.getenv("DB_PORT", 5432),
+        port=os.getenv("DB_PORT", 6543),
     )
 
 # Bulk insert (INSERT mode)
